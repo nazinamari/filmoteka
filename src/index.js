7bf5fa29d2f09851d965114efb4bb19db09297c7
Marina Nazina <nzyame@gmail.com>
2024-10-03 22:17:53 +0100

    webpack

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 import name from './module-1';
+import './scss/common.scss';
+
 console.log(name);
-console.log('Hello');
+console.log('Hello Це index');
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 	entry: './src/index.js',
@@ -6,4 +7,34 @@ module.exports = {
 		filename: 'main.js',
 		path: path.resolve(__dirname, 'build'),
 	},
+	module: {
+		rules: [
+			{
+				test: /\.scss$/i,
+				use: ['style-loader', 'css-loader', 'sass-loader'],
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: ['babel-loader'],
+			},
+		],
+	},
+	stats: 'errors-only', // Показувати тільки помилки при збірці
+	infrastructureLogging: {
+		level: 'error', // Показувати тільки помилки
+	},
+	plugins: [new HtmlWebpackPlugin()],
+	devServer: {
+		port: 1111,
+		open: true,
+		client: {
+			logging: 'warn', // Показувати попередження
+			overlay: true, // Вимкнути оверлей
+		},
+		hot: true,
+	},
 };
+
+// Показуємо, що сервер запущено
+console.log(`Dev Server is configured to run at http://localhost:1111`);
